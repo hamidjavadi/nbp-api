003faba18e608da8ce1c3fdc3f74a298e8bd9a46
Hamid Javadi <javadi.business@gmail.com>
2022-05-10 12:50:10 +0300

    Add date.service test

--- src/app/services/date.service.spec.ts
+++ src/app/services/date.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { DateService } from './date.service';
 
-describe('DateServiceService', () => {
+describe('DateService', () => {
   let service: DateService;
 
   beforeEach(() => {
@@ -11,6 +11,51 @@ describe('DateServiceService', () => {
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeInstanceOf(DateService);
   });
+
+  describe('formatDate method', () => {
+    it('should return YYYY-MM-DD foramt', () => {
+      const date1 = new Date(2022, 1, 1);
+      expect(service.formatDate(date1)).toBe('2022-01-01');
+
+      const date2 = new Date(2022, 11, 20);
+      expect(service.formatDate(date2)).toBe('2022-11-20');
+    });
+  });
+
+  describe('isValidDate method', () => {
+    it('should return boolean', () => {
+
+      let date;
+
+      expect(service.isValidDate('')).toBeFalse();
+      expect(service.isValidDate(date)).toBeFalse();
+
+      date = new Date();
+      expect(service.isValidDate(date)).toBeTrue();
+    });
+
+    it('should return false for the future date', () => {
+
+      const todayDate = new Date();
+      const tomorrow = new Date();
+
+      tomorrow.setDate(todayDate.getDate() + 1);
+
+      expect(service.isValidDate(tomorrow)).toBeFalse();
+      expect(service.isValidDate(todayDate)).toBeTrue();
+    });
+
+    it('should return false for the date longer 93 days ago', () => {
+
+      const todayDate = new Date();
+      const longer93DaysAgoDate = new Date();
+
+      longer93DaysAgoDate.setDate(todayDate.getDate() - 94);
+
+      expect(service.isValidDate(longer93DaysAgoDate)).toBeFalse();
+      expect(service.isValidDate(todayDate)).toBeTrue();
+    });
+  })
 });
